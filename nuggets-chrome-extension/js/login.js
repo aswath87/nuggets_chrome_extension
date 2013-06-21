e6f8dc9a9fa428e0831b7b4760effae2d99f6c6f
nathanwchan <nchan87@gmail.com>
2013-06-20 21:20:38 -0700

    chrome extension: register page

--- nuggets-chrome-extension/js/login.js
+++ nuggets-chrome-extension/js/login.js
@@ -5,6 +5,11 @@ function initialize() {
   validateLogin();
 }
 
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 function validateLogin() {
   var currentUser = Parse.User.current();
   if (currentUser)
@@ -49,6 +54,70 @@ $('#login-button').click(function()
     });
   }
   $('#login-button').disabled = false;
+});
+
+$('#register-button').click(function()
+{
+  $('#register-button').disabled = true;
+  if ($('#register-name').val() == "")
+  {
+    $('#register-error-1').css('display','block');
+    $('#register-error-2').css('display','none');
+    $('#register-error-3').css('display','none');
+    $('#register-error-4').css('display','none');
+    $('#register-name').focus();
+  }
+  else if (!validateEmail($('#register-email').val()))
+  {
+    $('#register-error-1').css('display','none');
+    $('#register-error-2').css('display','block');
+    $('#register-error-3').css('display','none');
+    $('#register-error-4').css('display','none');
+    $('#register-email').focus();
+  }
+  else if ($('#register-password').val().length < 6)
+  {
+    $('#register-error-1').css('display','none');
+    $('#register-error-2').css('display','none');
+    $('#register-error-3').css('display','block');
+    $('#register-error-4').css('display','none');
+    $('#register-password').focus();
+  }
+  else
+  {
+    $('#register-error-1').css('display','none');
+    $('#register-error-2').css('display','none');
+    $('#register-error-3').css('display','none');
+    $('#register-error-4').css('display','none');
+    var user = new Parse.User();
+    user.set("displayname", $('#register-name').val());
+    user.set("username", $('#register-email').val());
+    user.set("password", $('#register-password').val());
+    user.set("email", $('#register-email').val());
+    user.signUp(null, {
+      success: function(user) {
+        goToNuggetPage();
+      },
+      error: function(user, error) {
+        $('#register-error-4').html(error.message);
+        $('#register-error-4').css('display','block');
+        $('#register-email').focus();
+      }
+    });
+  }
+  $('#register-button').disabled = false;
+});
+
+$('#go-to-register').click(function()
+{
+  $('#login-div').css('display','none');
+  $('#register-div').css('display','block');
+});
+
+$('#go-to-login').click(function()
+{
+  $('#register-div').css('display','none');
+  $('#login-div').css('display','block');
 });
 
 });
